a0c52506e47071488bbd1f8008a4b313e8bc63d4
KeRoN00 <listwon.dev@gmail.com>
2023-11-07 19:16:51 +0100

    Added basic routing

--- acc-sync-app/src/main.tsx
+++ acc-sync-app/src/main.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from './pages/LoginPage.tsx';
+import AdminDashboard from './pages/admin/AdminDashboard.tsx';
+import UserDashboard from './pages/user/UserDashboard.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage/>,
+  },
+  {
+    path: "/user-dashboard",
+    element: <UserDashboard/>,
+  },
+  {
+    path: "/admin-dashboard",
+    element: <AdminDashboard/>,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
---
+++ acc-sync-app/src/pages/user/UserDashboard.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/ban-types */
+type Props = {}
+
+const UserDashboard = (props: Props) => {
+  return (
+    <div>UserDashboard</div>
+  )
+}
+
+export default UserDashboard
